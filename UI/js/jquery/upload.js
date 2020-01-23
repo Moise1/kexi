d4df5649b3003a039898564b17069ece902fdbac
Moise1 <moiseri01@gmail.com>
2020-01-23 21:11:33 +0200

    success alert for files upload

--- UI/js/jquery/upload.js
+++ UI/js/jquery/upload.js
@@ -1,8 +1,22 @@
 (()=>{
     document.querySelector('#uploadButton').addEventListener('click', ()=>{
-        return document.querySelector('#message').innerHTML = `<div class="alert alert-success fade in">
-        <div class="close close-alert" data-dismiss="alert" aria-hidden="true">&times;</div>
-            Files Successfully uploaded! Please refresh the page.
-        </div>`;
+       document.querySelector('.alert').show();
+    })
+
+    document.querySelector('button').addEventListener('click', ()=>{
+        document.querySelector('.alert').remove()
     })
 })();
+
+
+
+
+// $(document).ready(function(){
+//     $('#uploadButton').click(function(){
+//         $('.alert').show()
+//     }); 
+    
+//     $('button').click(function(){
+//         $(this).parents('div').remove();
+//     }); 
+// });
